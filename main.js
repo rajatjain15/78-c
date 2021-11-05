eedc0fd0a677e077b1f085449bbee0106f5c7359
rajatjain15 <79210039+rajatjain15@users.noreply.github.com>
2021-11-05 17:00:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var info=[
+  "img-2jpg",
+  "img-3jpg",
+  "img-4jpg",
+  "img-5jpg",  
+];
+function nextslide()
+{
+  var i=0;
+  if(i==4);
+    {i=0;}
+document.getElementById("aa")
+.src=info[i];
+i++;
+}
